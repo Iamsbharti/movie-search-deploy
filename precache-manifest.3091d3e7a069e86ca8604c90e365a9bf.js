f6cca4c8649b4f500f05fa7abd4c767830005e93
Saurabh <saurabhbharti9@gmail.com>
2020-05-24 14:18:31 +0530

    Updates

---
+++ precache-manifest.3091d3e7a069e86ca8604c90e365a9bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9361ac897770aaef0c3d708b23d7058d",
+    "url": "/movie-search-deploy/index.html"
+  },
+  {
+    "revision": "bfc7b28b5133e3518853",
+    "url": "/movie-search-deploy/static/css/main.ee308247.chunk.css"
+  },
+  {
+    "revision": "aa9ed749ffd47bb44ff1",
+    "url": "/movie-search-deploy/static/js/2.417f0475.chunk.js"
+  },
+  {
+    "revision": "bfc7b28b5133e3518853",
+    "url": "/movie-search-deploy/static/js/main.42a0113f.chunk.js"
+  },
+  {
+    "revision": "a18b8be24093f5704a85",
+    "url": "/movie-search-deploy/static/js/runtime~main.4d1a6019.js"
+  }
+]);
